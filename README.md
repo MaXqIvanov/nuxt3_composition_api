e921615b4b7193ee522b82c0e753ce63e7b39306
MaXqIvanov <maksivanov35@ya.ru>
2022-12-06 18:41:39 +0300

    added header components

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Nuxt3 Pinia CompositionApi
+
 # Nuxt 3 Minimal Starter
 
 Look at the [Nuxt 3 documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="header">
         <div class="info header__info">11</div>
-        <div class="profile header__profile"><Icon name="mdi:cart-outline"></Icon></div>
+        <div class="profile header__profile">
+            <Icon name="mdi:cart-outline"></Icon>
+            <div class="separate__line"></div>
+            <div class="products">
+                <div class="products__count">0 руб</div>
+                <div class="products__costs">0 шт.</div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,9 +19,46 @@
 <style lang="scss" scoped>
 $background_header: #282828;
 $color_header: white;
+.separate__line{
+    height: 40px;
+    width: 0.12vw;
+    min-width: 1px;
+    background: white;
+    margin-right: 14px;
+    margin-left: 10px;
+}
+.products__costs{
+    font-size: 1.1vw;
+    font-weight: 700;
+    @media(min-width: 1500px){
+        font-size: 0.7vw;
+    }
+}
+.products__count{
+    font-size: 1.3vw;
+    font-weight: 600;
 
+    @media(min-width: 1500px){
+        font-size: 0.9vw;
+    }
+}
+.products{
+    display: flex;
+    flex-direction: column;
+}
 .header__profile{
-    font-size: 2.5vw;
+    position: relative;
+    display: flex;
+    align-items: center;
+    svg{
+        font-size: 2.4vw;
+        cursor: pointer;
+    }
+    @media(min-width: 1500px){
+        svg{
+            font-size: 1.8vw;
+        }
+    }
 }
 .header{
     position: fixed;
