a4924c7ffbaeeae94788e2a7162344bfc4083abb
MaXqIvanov <maksivanov35@ya.ru>
2022-12-06 11:49:53 +0300

    first commit

---
+++ app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{new_var}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useHead } from './.nuxt/imports';
+  useHead({
+    title: 'Pizza Mafia',
+    meta: [
+      { name: 'description', content: 'Pizza Mafia - best pizza' }
+    ],
+    bodyAttrs: {
+      class: 'test'
+    },
+    script: [ { children: 'console.log(\'Hello world\')' } ]
+  })
+
+  const new_var = '123'
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
